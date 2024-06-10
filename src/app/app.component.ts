9280bcb4637dbb57a618b70bf5bd713c7a86fe78
F0oocuS <ihoryaremko@gmail.com>
2024-06-10 19:56:45 +0300

    add websocket client and output first test messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './shared/services/websocket.service';
 
 @Component({
 	selector: 'app-root',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 	styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+	public message: string = '';
+	public messages: string[] = [];
 	title = 'five-tribes';
 
-	public ngOnInit() {}
+	constructor(private websocketService: WebsocketService) {}
+
+	public ngOnInit(): void {
+		this.websocketService.getMessage().subscribe((message: string) => {
+			this.messages.push(message);
+		});
+	}
+
+	public sendMessage(): void {
+		this.websocketService.sendMessage(this.message);
+		this.message = '';
+	}
 }
